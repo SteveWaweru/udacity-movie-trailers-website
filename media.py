06cb3c13b90ecb26c0b3499e28df3e257ce1022e
steve <steviewez@gmail.com>
2016-03-20 17:29:10 +0300

    pep8 format styling

--- media.py
+++ media.py
@@ -1,17 +1,17 @@
 # Movie class that takes title, image url and youtube trailer url as the parameters
 class Movie:
-    '''
+    """
     Class to store details about movie object.
 
     Attributes:
         title (str): Title of the movie
         poster_image_url (str): The url for the poster for the movie
         trailer_youtube_url (str): The url for the youtube trailer for the movie
-    '''
+    """
+
     # Constructor to intialize the class with the arguments given.
     def __init__(self, movie_title, poster_image, trailer_youtube_url, genre):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube_url
         self.genre = genre
-
